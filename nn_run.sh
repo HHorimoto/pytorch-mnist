82b07b5a50ef1620ae026640d83abae2866f3ef6
HHorimoto <81685394+HHorimoto@users.noreply.github.com>
2025-03-09 19:49:28 +0900

    fix: run shell

--- nn_run.sh
+++ nn_run.sh
@@ -5,7 +5,7 @@
 now=`date "+%F_%T"`
 echo $now
 mkdir ./log/$now
-python ./nn_main.py $num 2>&1 | tee ./log/$now/log.txt
+python ./nn_main.py 2>&1 | tee ./log/$now/log.txt
 
 mv loss.png ./log/$now/
 mv model ./log/$now/
